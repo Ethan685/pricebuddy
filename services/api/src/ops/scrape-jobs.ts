e92d349f88f34435be0e169c15d591f0e9690973
Homz <wk8612@gmail.com>
2025-12-16 06:28:21 +0900

    ops: add scrape_jobs entrypoint

--- services/api/src/ops/scrape-jobs.ts
+++ services/api/src/ops/scrape-jobs.ts
@@ -1,8 +1,8 @@
 async function main() {
   console.log("scrape_jobs: start");
-  console.log("scrape_jobs: TODO connect scraper -> persist offers -> alerts");
   console.log("scrape_jobs: done");
 }
+
 main().catch((e) => {
   console.error(e);
   process.exit(1);
