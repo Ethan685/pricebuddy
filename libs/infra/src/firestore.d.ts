0add29ead533c851bf75c0475a62393f23f2e098
Homz <wk8612@gmail.com>
2025-12-15 22:23:53 +0900

    chore: add typecheck scripts

---
+++ libs/infra/src/firestore.d.ts
@@ -0,0 +1,3 @@
+import admin from "firebase-admin";
+export declare const firestore: admin.firestore.Firestore;
+//# sourceMappingURL=firestore.d.ts.map
--- libs/infra/src/firestore.js
+++ libs/infra/src/firestore.js
@@ -1,6 +1,18 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.firestore = void 0;
-const firebase_1 = require("./firebase");
-exports.firestore = firebase_1.firebase.firestore();
+const firebase_admin_1 = __importDefault(require("firebase-admin"));
+function ensureApp() {
+    if (!firebase_admin_1.default.apps.length) {
+        firebase_admin_1.default.initializeApp();
+    }
+    return firebase_admin_1.default.app();
+}
+exports.firestore = (() => {
+    ensureApp();
+    return firebase_admin_1.default.firestore();
+})();
 //# sourceMappingURL=firestore.js.map
