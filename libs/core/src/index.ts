31c2f8fa45c089241de889d516d10bb236654e0c
Homz <wk8612@gmail.com>
2025-12-16 05:43:49 +0900

    fix(core): remove merge conflict markers in exports

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -5,16 +5,9 @@ export * from "./domain/user";
 export * from "./domain/wallet";
 export * from "./domain/alert";
 export * from "./domain/deal";
+export * from "./domain/job";
 
-// API types
 export * from "./api/http";
 export * from "./api/search";
 export * from "./api/product-detail";
 export * from "./api/ext";
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> origin/main
-export * from "./domain/job";
