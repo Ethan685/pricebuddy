144bdc394da3c0f6298572c579dd8e0e9416a921
Homz <wk8612@gmail.com>
2025-12-15 23:42:28 +0900

    fix(flutter): remove google_sign_in usage and use firebase_auth provider

--- flutter_app/lib/services/auth_service.dart
+++ flutter_app/lib/services/auth_service.dart
@@ -1,31 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  final GoogleSignIn _googleSignIn = GoogleSignIn(
-    scopes: <String>['email'],
-  );
-
   User? get currentUser => _auth.currentUser;
 
   Future<UserCredential?> signInWithGoogle() async {
-    final GoogleSignInAccount? account = await _googleSignIn.signIn();
-    if (account == null) return null;
-
-    final GoogleSignInAuthentication auth = await account.authentication;
-
-    final OAuthCredential credential = GoogleAuthProvider.credential(
-      idToken: auth.idToken,
-      accessToken: auth.accessToken,
-    );
-
-    return _auth.signInWithCredential(credential);
+    final provider = GoogleAuthProvider();
+    provider.addScope('email');
+    return _auth.signInWithProvider(provider);
   }
 
   Future<void> signOut() async {
-    await _googleSignIn.signOut();
     await _auth.signOut();
   }
 }
