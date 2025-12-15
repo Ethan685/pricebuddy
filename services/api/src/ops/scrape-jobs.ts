8f7bcde1e8a66250733e9aa52e004e2586598ab9
Homz <wk8612@gmail.com>
2025-12-16 06:25:19 +0900

    ops(api): add scrape_jobs entrypoint

--- services/api/src/ops/scrape-jobs.ts
+++ services/api/src/ops/scrape-jobs.ts
@@ -1,8 +1,9 @@
 async function main() {
   console.log("scrape_jobs: start");
-  // TODO: connect queue -> fetch offers -> persist to Firestore
+  console.log("scrape_jobs: TODO connect scraper -> persist offers -> alerts");
   console.log("scrape_jobs: done");
 }
+
 main().catch((e) => {
   console.error(e);
   process.exit(1);
