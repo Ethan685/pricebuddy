d1855fc30456a694eb9d18bdbc67611766e19254
Homz <wk8612@gmail.com>
2025-12-15 22:41:05 +0900

    fix(pricing): export computePrice from index

--- services/pricing/src/index.ts
+++ services/pricing/src/index.ts
@@ -1,8 +1,1 @@
-// 임시로 비활성화 (Firebase Functions 배포를 위해)
-// export * from "./pricing-engine";
-// export * from "./forex/rates-store";
-// export * from "./data/tax-rules";
-// export * from "./data/shipping-rules";
-
-// 빈 export로 대체
-export {};
+export * from "./pricing-engine";
